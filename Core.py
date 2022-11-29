b3dcdd776bb21a0cc32d8964194ef857248c461f
sparshni <sparsh123nidhi@gmail.com>
2022-11-29 19:55:10 +0545

    This is the main code from Pyimagesearch USes tesseract OCR and heavy processing

--- Core.py
+++ Core.py
@@ -10,7 +10,6 @@ def cleanup_text(text):
     # strip out non-ASCII text so we can draw the text on the image
     # using OpenCV
     return "".join([c if ord(c) < 128 else "" for c in text]).strip()
-
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True,
